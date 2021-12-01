2a28f3d0d30fb4168d36e16a3f34974329165d5b
juanManuelGonzalezA00572003 <85131308+juanManuelGonzalezA00572003@users.noreply.github.com>
2021-12-01 07:56:51 -0600

    menos graficas.

--- Comunicacion/Proyecto/Proyecto.ino
+++ Comunicacion/Proyecto/Proyecto.ino
@@ -1,10 +1,10 @@
 #include "ESP8266WiFi.h"
 #include <WiFiClient.h> 
 #include <Servo.h>
-int DHpin = 4; // input/output pin
+
 int fotores = A0;
 int ledPin = 0;
-int servoPin = 2;
+int servoPin = 14;
 int motorPinR = 16;
 int motorPinL = 5;
 
@@ -31,6 +31,10 @@ const char *pass = "57142857";
 char host[48];
 String strhost = "192.168.41.38";
 String strurl = "/enviardatos.php";
+
+
+float tempSum, humSum, lightSum, startLight= 0, startTemp = 0, startHum = 0, relTemp, relHum, relLight;
+int tempCount, humCount, lightCount;
 
 
 //--------------------------------------------------------------------------------
@@ -87,81 +91,95 @@ void setup()
    Serial.println(ssid);
    WiFi.begin(ssid, pass);
 
-  while (WiFi.status() != WL_CONNECTED) 
+int tries = 0;
+
+  while (WiFi.status() != WL_CONNECTED && tries<20) 
  {
         delay(500);
         Serial.print(".");
+        tries++;
  }
   Serial.println("");
   Serial.println("WiFi connected");
 
   dht.begin();
+
 }
 
 
 
 void loop()
 {
+
+
+  
+
+  
   // Leemos la humedad relativa
-  float h = dht.readHumidity();
+  float hum = dht.readHumidity();
   // Leemos la temperatura en grados centígrados (por defecto)
-  float t = dht.readTemperature();
-  
-  
-  Serial.print("Humdity = ");
-  Serial.print(h); //Displays the integer bits of humidity;
+  float temp = dht.readTemperature();
+
+  float light = analogRead(fotores);
+
+  tempSum+=temp;
+  humSum+=hum;
+  lightSum+=light;
+
+  tempCount++;
+  humCount++;
+  lightCount++;
+
+    if(startHum>0 && startTemp>0 && startLight>0){
+
+    relHum = ((humSum/humCount)/startHum)*100;
+    relTemp = ((tempSum/tempCount)/startTemp)*100;
+    relLight = ((lightSum/lightCount)/startLight)*100;
+    
+    Serial.print("Humdity = ");
+  Serial.print(relHum); //Displays the integer bits of humidity;
   Serial.print("%\t");
   Serial.print("Temperature = ");
-  Serial.print(t); //Displays the integer bits of temperature;
+  Serial.print(relTemp); //Displays the integer bits of temperature;
   Serial.print("C\t");
-
-
-  float luz = analogRead(fotores);
   Serial.print("Light: ");
-  Serial.println(luz);
-  if (luz < 60)
-  {
-    digitalWrite(ledPin, HIGH);
+  Serial.println(relLight);
+  Serial.println("");
   }
 
-  else
-  {
+  servo.write(70);
 
-    digitalWrite(ledPin, LOW);
-  }
 
-  if (isnan(h) || isnan(t)) {
-      Serial.println("Error obteniendo los datos del sensor DHT11");
-      return;
-    } else {
-        if (h > 50)
-  {
-    servo.write(180);
-  }
-  else
-  {
-    servo.write(0);
-  }
-
-  if (t > 22.1)
-  {
+  if(relTemp > 101){
     digitalWrite(motorPinR, HIGH);
-  }
-  else
-  {
+    digitalWrite(motorPinL, HIGH);
+  } else {
     digitalWrite(motorPinR, LOW);
+    digitalWrite(motorPinL, LOW);
   }
 
-    }
+  Serial.println(servo.read());
 
+  if(startHum == 0 && hum > 0){
+    startHum = hum;
+  }
+
+  if(startTemp == 0 && temp > 0){
+    startTemp = temp;
+  }
+
+  if(startLight == 0 && light > 0){
+    startLight = light;
+  }
 
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= 10000) { //envia la temperatura cada 10 segundos
+  if (currentMillis - previousMillis >= 3000) { //envia la temperatura cada 10 segundos
     previousMillis = currentMillis;
-    //int analog = analogRead(17);
-    //float temp = analog*0.322265625;
-    //Serial.println(temp);
-    enviardatos("&Temperatura=" + String(t, 2) + "&Humedad=" + String(h, 2) + "&Luz=" + String(luz, 2));
+    
+
+  
+    enviardatos("&Temperatura=" + String(tempSum/tempCount, 2) + "&Humedad=" + String(humSum/humCount, 2) + "&Luz=" + String(lightSum/lightCount, 2));
+    tempSum = humSum = lightSum = tempCount = humCount = lightCount = 0;
   }
 }
